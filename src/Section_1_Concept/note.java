93ee2c0b7ea03659d79596cb6ac66c96fd089e78
Ryan-Nam <alfofh7060@gmail.com>
2021-09-24 22:45:29 +1000

    First Commit:

---
+++ src/Section_1_Concept/note.java
@@ -0,0 +1,21 @@
+package Section_1_Concept;
+
+public class note {
+    /*
+    *  What is Data Structure?
+    *  - Data Structure: A template that enables using Data efficiently.
+    *  It can be examed by the Time Complexity and Space Complexity
+    *
+    *  What is Time Complexity?
+    * - Time efficiency of certain Data structure
+    * - Less Time complexity is good data structure
+    * - Ex) Let's say, if you organize books well in shelf, you will find the book you are looking for easily later.
+    *  same as well, in this case, you are reducing time-complexity by using book shelf (Data structure)
+    *
+    * What is Space Complexity?
+    * */
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
