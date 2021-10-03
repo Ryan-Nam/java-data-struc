395d1b0185abc6a7733a57a05879ec8d72c04547
Ryan-Nam <alfofh7060@gmail.com>
2021-10-03 16:12:33 +1000

    Add ArrayList

---
+++ src/Section_4_List/Q1_ArrayList.java
@@ -0,0 +1,21 @@
+package Section_4_List;
+
+import java.util.ArrayList;
+
+public class Q1_ArrayList {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        System.out.printf("list = %s\n", list.toString());
+        list.add(4);
+        list.add(8);
+        list.add(12);
+        System.out.printf("list = %s\n", list.toString());
+        list.add(1, 10);
+        list.add(1, 100);
+        System.out.printf("list = %s\n", list.toString());
+        list.remove(2);
+        list.remove(3);
+        System.out.printf("list = %s\n", list.toString());
+        System.out.printf("size: %d\n", list.size());
+    }
+}
