c50571746ee171c8594353289babc02505f05f4b
Ryan-Nam <alfofh7060@gmail.com>
2021-10-14 22:26:11 +1000

    Add

---
+++ src/Section_6_Tree/Q2_DFS_Left_Right.java
@@ -0,0 +1,55 @@
+package Section_6_Tree;
+
+public class Q2_DFS_Left_Right {
+    public static void main(String[] args) {
+        // 트리 생성 및 구조화
+        MyTree tree7 = new MyTree(77, null, null);
+        MyTree tree6 = new MyTree(66, null, null);
+        MyTree tree5 = new MyTree(55, null, null);
+        MyTree tree4 = new MyTree(44, null, null);
+        MyTree tree3 = new MyTree(33, tree6, tree7);
+        MyTree tree2 = new MyTree(22, tree4, tree5);
+        MyTree tree1 = new MyTree(11, tree2, tree3);
+
+        // 순회 출력
+        tree1.printPreorder(); // 전위
+    }
+}
+
+class MyTree {
+    // 최상위 노드
+    private Node root;
+
+    // 생성자
+    public MyTree(int data, MyTree leftSubtree, MyTree rightSubtree) {
+        // 루트 노드 생성
+        this.root = new Node(data, null, null);
+
+        // 서브 트리 연결
+        root.leftChild = (leftSubtree == null) ? null : leftSubtree.root;
+        root.rightChild = (rightSubtree == null) ? null : rightSubtree.root;
+    }
+
+    // 전위 순회
+    public void printPreorder() {
+        System.out.print("preorder: ");
+        _preorder(root); // 재귀 호출
+        System.out.println();
+    }
+
+    private void _preorder(Node parent) {
+        /* 1: 재귀적 전위 순회를 구현하시오. */
+    }
+}
+
+class Node {
+    int data; // 데이터
+    Node leftChild; // 왼쪽 자식 노드
+    Node rightChild; // 오른쪽 자식 노드
+
+    public Node(int data, Node leftChild, Node rightChild) {
+        this.data = data;
+        this.leftChild = leftChild;
+        this.rightChild = rightChild;
+    }
+}
