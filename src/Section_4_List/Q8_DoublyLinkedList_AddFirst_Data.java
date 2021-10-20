f04816b63c51d1c61dbc1cfd2603593219009563
Ryan-Nam <alfofh7060@gmail.com>
2021-10-20 23:14:32 +1000

    Add

--- src/Section_4_List/Q8_DoublyLinkedList_AddFirst_Data.java
+++ src/Section_4_List/Q8_DoublyLinkedList_AddFirst_Data.java
@@ -1,4 +1,53 @@
 package Section_4_List;
 
 public class Q8_DoublyLinkedList_AddFirst_Data {
+
+
+    public static void main(String[] args) {
+        DoublyLinkedList list = new DoublyLinkedList();
+        list.addFirst(11);
+        list.addFirst(22);
+        list.addFirst(33);
+        System.out.println(list.toString());
+        System.out.println(list.toStringBackward());
+    }
+}
+class DoublyLinkedList {
+    private Node head;
+    private Node tail;
+    public DoublyLinkedList() {
+        this.head = null;
+        this.tail = null;
+    }
+    public void addFirst(int data) {
+        /* 해당 메소드를 구현하시오. */
+    }
+    public String toString() {
+        StringBuffer sbuf = new StringBuffer("list: head -> ");
+        Node temp = head;
+        while (temp != null) {
+            sbuf.append(temp.data + " -> ");
+            temp = temp.right;
+        }
+        return sbuf.append("null").toString();
+    }
+    public String toStringBackward() {
+        StringBuffer sbuf = new StringBuffer("list: tail -> ");
+        Node temp = tail;
+        while (temp != null) {
+            sbuf.append(temp.data + " -> ");
+            temp = temp.left;
+        }
+        return sbuf.append("null").toString();
+    }
+    class Node {
+        int data;
+        Node left;
+        Node right;
+        private Node(int data, Node left, Node right) {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+    }
 }
